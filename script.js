6a0e2f35a9e93f27adb7a91f629ca96dd5c5ea3b
VolviX <20700808+VolviX@users.noreply.github.com>
2025-01-04 20:02:22 +0300

    Add styling

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const myLibrary = [];
+const myLibrary = [
+    new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true),
+    new Book("Brave New World", "Aldous Huxley", 268, false),
+    new Book("Pride and Prejudice", "Jane Austen", 279, true),
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,3 +19,11 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+const main = document.querySelector('.main');
+
+myLibrary.forEach((book) => {
+    const display = document.createElement('li');
+    display.textContent = book.getInfo();
+    main.appendChild(display); 
+});
